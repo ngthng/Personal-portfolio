9bc9c0b9ce7e4725373af63de5c6fc272a57587b
ngthng <114264706+ngthng@users.noreply.github.com>
2024-08-06 23:28:58 -0700

    test run instagram embed

--- data/notesHome.js
+++ data/notesHome.js
@@ -8,7 +8,7 @@ const welcomeNote = new Note(
     </p>
     <ul>
       <li>Creating this webpage!</li>
-      <li>Adding carousels from Instagram @yootheocoooks</li>
+      <li>Adding carousels from Instagram <a href="https://www.instagram.com/yootheocoooks/" target="_blank">@yootheocoooks</a></li>
       <li>Exploring baking with friends</li>
     </ul>
   `,
@@ -52,7 +52,8 @@ const navNote = new Note(
 
 const instagramTwo = new Note (
   'instagram2',
-  ``,
+  `
+  <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://scontent.cdninstagram.com/v/t51.29350-15/454270185_1217746615825410_4394977177350173741_n.webp?stp=dst-jpg_e35_s640x640_sh0.08&amp;efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE0NDAuc2RyLmYyOTM1MCJ9&amp;_nc_ht=scontent.cdninstagram.com&amp;_nc_cat=106&amp;_nc_ohc=1p1-DPbu_CAQ7kNvgEqNmBo&amp;edm=APs17CUBAAAA&amp;ccb=7-5&amp;oh=00_AYBr8SDslCKfvLeysi8i0pkFCNmkp0xLbpgF8H6pMoL3Zw&amp;oe=66B8D21B&amp;_nc_sid=10d13b">`,
   4,
   4
 )
--- scripts/cooking-blog.js
+++ scripts/cooking-blog.js
@@ -1,5 +1,3 @@
 import { homeNote } from "../data/notesHome.js";
 
-console.log(homeNote);
-
 homeNote.renderNotes(document.querySelector('.js-note-container'))
